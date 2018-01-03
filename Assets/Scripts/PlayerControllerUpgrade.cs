b69bf654b04ad912ddb5abb26a57d2f3b99ea945
Bhaskar8sharma <bhaskarsharma0789@gmail.com>
2018-01-03 10:17:06 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerControllerUpgrade.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerUpgrade : MonoBehaviour
+{
+    public float speed = 4.0f;
+    private AnimationCurve animationCurve;
+
+    private float distanceToTravel;
+    private float timeToTravel;
+    private float myTime;
+    private bool showCutscene;
+    private PointSetter gamePoints;
+    private int currentPoint;
+    private int maxPoints;
+    private Vector3 start;
+    private Vector3 end;
+
+    // Use this for initialization
+    void Start()
+    {
+        gamePoints = GameObject.FindGameObjectWithTag("GameController").GetComponent<PointSetter>();
+        animationCurve = gamePoints.PlayerMovement;
+        showCutscene = false;
+        currentPoint = 0;
+        maxPoints = 0;
+        myTime = 0.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !gamePoints.HidePlayer && !showCutscene)
+        {
+            showCutscene = true;
+            maxPoints = gamePoints.Targets.Count;
+        }
+
+        if (currentPoint > gamePoints.Targets.Count - 2)
+        {
+            transform.position = gamePoints.Targets[currentPoint].transform.position;
+            showCutscene = false;
+            currentPoint = 0;
+            myTime = 0.0f;
+        }
+        
+        if (showCutscene)
+        {
+            start = gamePoints.Targets[currentPoint].transform.position;
+            end = gamePoints.Targets[currentPoint + 1].transform.position;
+            distanceToTravel = Vector3.Distance(start, end);
+            timeToTravel = distanceToTravel / speed;
+
+            myTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(start, end, animationCurve.Evaluate(myTime / timeToTravel));
+            transform.right = (end - start).normalized;
+            if(myTime >= timeToTravel)
+            {
+                currentPoint++;
+                myTime = 0.0f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PointSetter.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointSetter : MonoBehaviour
+{
+
+    private List<GameObject> targets;
+    public GameObject target;
+    public GameObject Player;
+    private bool hidePlayer;
+    public AnimationCurve PlayerMovement;
+    public Text message;
+
+    public List<GameObject> Targets
+    {
+        get
+        {
+            return targets;
+        }
+
+        set
+        {
+            targets = value;
+        }
+    }
+
+    public bool HidePlayer
+    {
+        get
+        {
+            return hidePlayer;
+        }
+
+        set
+        {
+            hidePlayer = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        targets = new List<GameObject>();
+        HidePlayer = true;
+        message.text = "left mouse click to add points";
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Setup();
+        }
+        if (targets.Count > 2 && HidePlayer)
+        {
+            ReadyForDeployment();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetPoints();
+        }
+    }
+
+    public void ReadyForDeployment()
+    {
+        message.text = "Use left mouse click to add points\nPress [Spacebar] to start sequence";
+        //startButton.GetComponent<Renderer>().enabled = true;
+        Instantiate(Player, targets[0].transform.position, Quaternion.identity);
+        HidePlayer = false;
+    }
+
+    public void ResetPoints()
+    {
+        message.text = "Use left mouse click to add points\nPress R to reset points";
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+        foreach (GameObject gameObject in targets)
+        {
+            Destroy(gameObject);
+        }
+        targets.Clear();
+        HidePlayer = true;
+    }
+
+    public void Setup()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            Debug.Log(hit.transform.name);
+        }
+        else if (!Physics.Raycast(ray) && targets.Count < 10)
+        {
+            GameObject newTarget = Instantiate(target, ray.origin, Quaternion.identity);
+            targets.Add(newTarget);
+        }
+    }
+}
